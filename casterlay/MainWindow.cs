7092d5bc3cb4248193d6a1f93afd084c4bb1640c
xolgorim <xolgorim@googlemail.com>
2017-02-08 16:29:18 +0100

    Build stuff

--- casterlay/MainWindow.cs
+++ casterlay/MainWindow.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using casterlay;
+using Gdk;
 using Gtk;
 
 public partial class MainWindow : Gtk.Window
@@ -6,11 +9,48 @@ public partial class MainWindow : Gtk.Window
 	public MainWindow() : base(Gtk.WindowType.Toplevel)
 	{
 		Build();
+		Gtk.FileFilter filter = new FileFilter();
+		filter.Name = "Images";
+		filter.AddMimeType("image/png");
+		filter.AddMimeType("image/jpeg");
+		filter.AddMimeType("image/gif");
+		filter.AddPattern("*.png");
+		filter.AddPattern("*.jpg");
+		filter.AddPattern("*.gif");
+		filter.AddPattern("*.tif");
+		filter.AddPattern("*.xpm");
+		filechooserwidget1.Filter = filter;
 	}
 
 	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
 	{
 		Application.Quit();
 		a.RetVal = true;
+	}
+
+	protected void OnButton2Clicked(object sender, EventArgs e)
+	{
+		spinbutton1.Spin(SpinType.Home, 1f);
+		spinbutton2.Spin(SpinType.Home, 1f);
+	}
+
+	Pixbuf img = null;
+
+	protected void OnFilechooserwidget1SelectionChanged(object sender, EventArgs e)
+	{
+		
+		label5.Text = "Selected: "+filechooserwidget1.Filename;
+		if (filechooserwidget1.PreviewFilename != null)
+		{
+			img = new Pixbuf(filechooserwidget1.Filename);
+		}
+	}
+
+	protected void OnButton8Clicked(object sender, EventArgs e)
+	{
+		if (img != null)
+		{
+			new Overlay(img);
+		}
 	}
 }
---
+++ casterlay/Overlay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Gdk;
+using Gtk;
+
+namespace casterlay
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class Overlay : Gtk.Window
+	{
+		Pixbuf img;
+		float factor;
+
+		public Overlay(Pixbuf img):base("Casterlay")
+		{
+			this.Decorated = false;
+			this.KeepAbove = true;
+			this.Opacity = 0f;
+			Build();
+			this.img = img;
+			buildOverlay();
+		}
+
+		void buildOverlay()
+		{
+			Style style = this.Style;
+			Pixmap map, mask;
+			factor = Screen.Width / img.Width;
+			img = img.ScaleSimple(Screen.Width, (int)(img.Height * factor), InterpType.Bilinear);
+			Console.WriteLine("Width: " + img.Width + " Height: " + img.Height + " Factor: " + factor);
+			img.RenderPixmapAndMask(out map, out mask, 255);
+			image3.Pixbuf = img;
+			SetDefaultSize(img.Width, img.Height);
+			SetPosition(WindowPosition.Center);
+			style.SetBgPixmap(StateType.Normal, map);
+			this.Style = style;
+			Show();
+		}
+}
+}
